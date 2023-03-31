78da96782d23d9b78bee3b910cee84188b6dc543
Amis333 <vlad172348@gmail.com>
2023-03-31 22:52:48 +0300

    1 commit insertion sort

---
+++ 427/Shumilov Vlad/[2].py
@@ -0,0 +1,27 @@
+# 4 - сортировка вставкой
+# 6 - сортировка деревом
+# 17 - битонная сортировка
+# 10 - быстрая сортировка
+import random as rnd
+
+
+arr_N = []
+
+for i in range(1000):
+    arr_N.append(i)
+
+rnd.shuffle(arr_N)
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+            arr[j+1] = key
+    return arr
+
+
+print(insertion_sort(arr_N))
