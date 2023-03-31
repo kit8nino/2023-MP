f0a714166f1c118f8926c021f8642945a453e255
Amis333 <vlad172348@gmail.com>
2023-03-31 23:25:45 +0300

    [2] quick sort

--- 427/Shumilov Vlad/[2].py
+++ 427/Shumilov Vlad/[2].py
@@ -3,16 +3,22 @@
 # 17 - битонная сортировка
 # 10 - быстрая сортировка
 import random as rnd
+import numpy as np
 
 
 arr_N = []
 arr_R = []
 
+# №1 список целых чисел от 0 до 999999 (ну почти)
 for i in range(1000):
     arr_N.append(i)
-
-
 rnd.shuffle(arr_N)
+
+# №2 список из (почти) 99999 случайных вещественных чисел в диапазоне [-1, 1]
+for i in range(-1000, 1000):
+    arr_R.append(i / 1000)
+print(arr_R)
+rnd.shuffle(arr_R)
 
 
 def insertion_sort(arr):
@@ -26,7 +32,24 @@ def insertion_sort(arr):
     return arr
 
 
+def quick_sort(arr, low, high):
+    if low >= high:
+        return
+    else:
+        rnd_start = rnd.choice(arr[low:high + 1])
+        i = low
+        j = high
+        while i <= j:
+            while arr[i] > rnd_start:
+                i += 1
+            while arr[j] < rnd_start:
+                j -= 1
+            if i <= j:
+                arr[i], arr[j] = arr[j], arr[i]
+                i += 1
+                j -= 1
+                quick_sort(arr, low, j)
+                quick_sort(arr, i, high)
 
 
-
-print(insertion_sort(arr_N))
+print(quick_sort(arr_R, 0, len(arr_R) - 1))
