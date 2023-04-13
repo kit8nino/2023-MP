522669708e20954a1da428b2a53306875fdaff76
Amis333 <vlad172348@gmail.com>
2023-04-13 15:46:50 +0300

    [1]

--- 427/Shumilov Vlad/[1].py
+++ 427/Shumilov Vlad/[1].py
@@ -32,14 +32,14 @@ def avg_mark(obj):
     return sum(obj.values()) / len(obj)
 
 
-print(f'Средняя оценка аттестата {avg_mark(certificate)} балла')
+print(f'Средняя оценка аттестата {avg_mark(certificate)} балла \n')
 
 
 # unique names
 
 
 uniqueNames = set(familyNames)
-print(f'Уникальные имена: {uniqueNames}')
+print(f'Уникальные имена: {uniqueNames} \n')
 
 
 # total length of all item names
@@ -52,7 +52,7 @@ def total_length(obj):
     return total
 
 
-print(f"Общая длина названий предметов: {total_length(certificate)} символов")
+print(f"Общая длина названий предметов: {total_length(certificate)} символов \n")
 
 # unique char
 
@@ -66,7 +66,7 @@ def unique_char(obj):
     return uniqueChar
 
 
-print(f'Уникальные символы: {unique_char(certificate)}')
+print(f'Уникальные символы: {unique_char(certificate)} \n')
 
 # kiwa name in bin
 
@@ -76,8 +76,7 @@ def binary_name(str):
     return binary
 
 
-print(f'Имя в двоичном представлении: {binary_name(kiwaName)}')
-
+print(f'Имя в двоичном представлении: {binary_name(kiwaName)} \n')
 
 # reverse sort
 
@@ -89,7 +88,7 @@ def reverse_list(list):
 
 reverseFamily = reverse_list(familyNames)
 
-print(f'Реверсивый список имен: {reverseFamily}')
+print(f'Реверсивый список имен: {reverseFamily} \n')
 
 # date time
 
@@ -98,9 +97,10 @@ def get_birth_days_out(list):
     return (dt.datetime.today() - dt.datetime(day=list[1], month=list[2], year=list[3])).days
 
 
-print(f"Дней прошло: {get_birth_days_out(data)}")
+print(f"Дней прошло: {get_birth_days_out(data)} \n")
 
-print("Введите индекс, '-1' для выхода")
+print("Введите индекс, '-1' для выхода \n")
+
 things = queue.Queue()
 i = int(input())
 while i != -1:
@@ -111,9 +111,7 @@ while not things.empty():
     print(things.get(), end=',')
 print()
 
-
 # №9
-
 
 AztecNames = ['Tenoch', 'Acamapochtli', 'Huitzilihitl', 'Chimalpopoca', 'Xihuitl Temoc',
             'Itzcoatl', 'Moctezuma I', 'Atotoztli', 'Axayacatl', 'Tizoc', 'Ahuitzotl',
@@ -121,17 +119,49 @@ AztecNames = ['Tenoch', 'Acamapochtli', 'Huitzilihitl', 'Chimalpopoca', 'Xihuitl
             'Xochiquentzin', 'Huanitzin', 'Tehuetzquititzin', 'Cecetzin', 'Cipac']
 
 index = (int(data[1]) + int(data[2])**2 + int(data[3])) % 21 + 1
-
-print(f'{AztecNames[index]} - имя ацтека под индексом {index}')
+print(f'{AztecNames[index]} - имя ацтека под индексом {index} \n')
 
 
 def replace_family_name(list):
     a = int(input(f'Введите число от 0 до {len(list)} : '))
     if a < 0 or a > len(list):
-        print("Нет доступного имени")
+        print("Нет доступного имени \n")
     else:
         list[a] = AztecNames[index]
-        print(f'Обновленный список {list}')
+        print(f'Обновленный список {list} \n')
 
 
 replace_family_name(reverseFamily)
+
+
+# №10
+
+def get_linked_list(arr):
+    lst = []
+    for i in range(len(arr)):
+        ls = [arr[i], i+1]
+        lst.append(ls)
+        ls = []
+    return lst
+
+
+# №11 Генератор аликвотной последовательности
+
+
+def aliquot(num):
+    d = []
+    aliquotPos = [num]
+    while num >= 1:
+        for i in range(1, num):
+            if num == 1:
+                aliquotPos.append(0)
+            elif num % i == 0:
+                d.append(i)
+        summ = sum(d)
+        aliquotPos.append(summ)
+        num = summ
+        d = []
+    return aliquotPos
+
+
+print("Aликвотная последовательность:", aliquot(10))
